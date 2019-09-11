06779d1e9b2b536d2d5bbfe542289f728aa79b41
MelodicNoob <jryan8831@gmail.com>
2019-09-11 14:32:23 +0800

    Started to implement the login form

--- functions.php
+++ functions.php
@@ -45,3 +45,38 @@ require_once(get_template_directory().'/functions/translation/translation.php');
 
 // Customize the WordPress admin
 // require_once(get_template_directory().'/functions/admin.php'); 
+
+function alumnus_login_manager_init() {
+    add_shortcode( 'alumnus-login-form', 'alumnus_login_form' );
+}
+
+add_action('init', 'alumnus_login_manager_init');
+
+// Generate the html content for alumnus login
+function alumnus_login_form() {
+    if ( !is_user_logged_in() ) {
+        $args = array(
+            'echo' => false,
+            'remember' => true,
+            'redirect' => get_permalink(),
+            'form_id' => 'alumnus_login',
+            'id_username' => 'alumnus_user_login',
+            'id_password' => 'almunus_password_login',
+            'id_remember' => 'alumnus_remember_me',
+            'id_submit' => 'alumnus_submit',
+            'label_username' => __( 'Enter your username'),
+            'label_password' => __( 'Password'),
+            'label_remember' => __( 'Remember Me' ),
+            'label_log_in' => __( 'Login' ),
+            'value_username' => '',
+            'value_remember' => false
+        );
+        $output = wp_login_form( $args );
+    } else {
+        $output = '<h3>You\'re logged in</h3>';
+    }
+    return $output;
+}
+
+add_filter('widget_text', 'do_shortcode');
+
