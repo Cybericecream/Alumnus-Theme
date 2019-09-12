84c7a8bf78d2f13bc65d4c1b6cf5376e3408905e
MelodicNoob <jryan8831@gmail.com>
2019-09-13 07:38:16 +0800

    Updated Alumnus register form widget

--- widgets/Alumnus_Register_Form.php
+++ widgets/Alumnus_Register_Form.php
@@ -13,19 +13,21 @@ class Alumnus_Register_Form extends WP_Widget {
     echo $args['before_widget'];
     if (!is_user_logged_in()) {
       echo '
-      <form method="POST" class="grid-x" action="' . esc_url( site_url( 'wp-login.php?action=register' )) .'">
-        <div class="loginTile__title cell small-12 text-center">
-          <div class="active">
-            <h2>Register for the Alumni</h2>
+      <div class="grid-container full">
+        <form method="POST" class="grid-x" action="' . esc_url( site_url( 'wp-login.php?action=register' )) .'">
+          <div class="loginTile__title cell small-12 text-center">
+            <div class="active">
+              <h2>Register for the Alumni</h2>
+            </div>
           </div>
-        </div>
-        <div class="loginTile__body cell large-4 large-offset-4 medium-8 medium-offset-2 small-12">
-          <input type="text" placeholder="Username" class="loginTile__input" name="user_login">
-          <input type="email" placeholder="Email" class="loginTile__input" name="user_email">
-          <button type="reset" class="btn">Clear</button>
-          <button type="submit" class="btn primaryButton">Register</button>
-        </div>
-      </form>
+          <div class="loginTile__body cell large-4 large-offset-4 medium-8 medium-offset-2 small-12">
+            <input type="text" placeholder="Username" class="loginTile__input" name="user_login">
+            <input type="email" placeholder="Email" class="loginTile__input" name="user_email">
+            <button type="reset" class="btn">Clear</button>
+            <button type="submit" class="btn primaryButton">Sign Up</button>
+          </div>
+        </form>
+      </div>
       ';
     }
     echo $args['after_widget'];
