f054942be972582878ee1057d7efaf432d3bb912
MelodicNoob <jryan8831@gmail.com>
2019-09-13 07:23:53 +0800

    Updated Almunus Login Form

--- widgets/Alumnus_Login_Form.php
+++ widgets/Alumnus_Login_Form.php
@@ -13,22 +13,20 @@ class Alumnus_Login_Form extends WP_Widget {
     echo $args['before_widget'];
       if (!is_user_logged_in()) {
         echo '
-          <form method="POST" class="grid-x grid-padding-x first" action="' . esc_url( site_url( 'wp-login.php', 'login_post' )) . '">
-              <div class="loginTile__title cell small-12 grid-x text-center">
-                  <div class="small-12 active">
-                    <h2>Login to the Alumni</h2>
-                  </div>
-              </div>
-              <div class="loginTile__body columns cell large-2 large-offset-5">
-                  <input type="text" placeholder="Email" class="loginTile__input" name="log">
-                  <input type="password" placeholder="Password" class="loginTile__input" name="pwd">
-                  <button type="reset" class="btn">Clear Form</button>
-                  <button type="submit" class="btn primaryButton">Login</button>
-              </div>
-          </form>
+        <form method="POST" class="grid-x" action="' . esc_url( site_url( 'wp-login.php' )) . '">
+          <div class="loginTile__title cell small-12 text-center">
+            <div class="active">
+              <h2>Login to the Alumni</h2>
+            </div>
+          </div>
+          <div class="loginTile__body cell large-4 large-offset-4 medium-8 medium-offset-2 small-12">
+              <input type="text" placeholder="Email" class="loginTile__input" name="log">
+              <input type="password" placeholder="Password" class="loginTile__input" name="pwd">
+              <button type="reset" class="btn">Clear</button>
+              <button type="submit" class="btn primaryButton">Login</button>
+          </div>
+        </form>
         ';
-      } else {
-        echo '<p>You\'re logged in.</p>';
       }
       echo $args['after_widget'];
   }
