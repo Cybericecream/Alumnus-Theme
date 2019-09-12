eaff2b3ec5a473d1694612c62981813f4b63a07e
MelodicNoob <jryan8831@gmail.com>
2019-09-13 07:39:09 +0800

    Updated Alumnus login form widget

--- widgets/Alumnus_Login_Form.php
+++ widgets/Alumnus_Login_Form.php
@@ -11,25 +11,25 @@ class Alumnus_Login_Form extends WP_Widget {
 
   public function widget( $args, $instance ) {
     echo $args['before_widget'];
-      if (!is_user_logged_in()) {
-        echo '
-        <div class="grid-container full">
-          <form method="POST" class="grid-x" action="' . esc_url( site_url( 'wp-login.php' )) . '">
-            <div class="loginTile__title cell small-12 text-center">
-              <div class="active">
-                <h2>Login to the Alumni</h2>
-              </div>
+    if (!is_user_logged_in()) {
+      echo '
+      <div class="grid-container full">
+        <form method="POST" class="grid-x" action="' . esc_url( site_url( 'wp-login.php' )) . '">
+          <div class="loginTile__title cell small-12 text-center">
+            <div class="active">
+              <h2>Login to the Alumni</h2>
             </div>
-            <div class="loginTile__body cell large-4 large-offset-4 medium-8 medium-offset-2 small-12">
-                <input type="text" placeholder="Email" class="loginTile__input" name="log">
-                <input type="password" placeholder="Password" class="loginTile__input" name="pwd">
-                <button type="reset" class="btn">Clear</button>
-                <button type="submit" class="btn primaryButton">Login</button>
-            </div>
-          </form>
-        </div>
-        ';
-      }
-      echo $args['after_widget'];
+          </div>
+          <div class="loginTile__body cell large-4 large-offset-4 medium-8 medium-offset-2 small-12">
+              <input type="text" placeholder="Email" class="loginTile__input" name="log">
+              <input type="password" placeholder="Password" class="loginTile__input" name="pwd">
+              <button type="reset" class="btn">Clear</button>
+              <button type="submit" class="btn primaryButton">Login</button>
+          </div>
+        </form>
+      </div>
+      ';
+    }
+    echo $args['after_widget'];
   }
 }
