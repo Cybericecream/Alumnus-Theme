db86523b8fc301d86d40e116e05e56cfc976c958
MelodicNoob <jryan8831@gmail.com>
2019-09-15 12:56:27 +0800

    Updated Alumnus login form

--- widgets/Alumnus_Login_Form.php
+++ widgets/Alumnus_Login_Form.php
@@ -5,30 +5,31 @@ class Alumnus_Login_Form extends WP_Widget {
     $widget_options = array(
       'description' => 'Alumnus Login Form which can be placed anywhere'
     );
-    parent::__construct( 'alumnus_login_form', 'Alumnus Login Form', $widget_options);
+    parent::__construct( 'Alumnus_Login_Form', 'Alumnus Login Form', $widget_options);
   }
 
   public function widget( $args, $instance ) {
     echo $args['before_widget'];
-    if (!is_user_logged_in()) {
-      echo '
-      <div class="grid-container full">
-        <form method="POST" class="grid-x" action="' . esc_url( site_url( 'wp-login.php' )) . '">
-          <div class="loginTile__title cell small-12 text-center">
-            <div class="active">
-              <h2>Login to the Alumni</h2>
+    echo '
+      <div class="grid-container">
+        <form name="loginForm" id="loginForm" method="POST" action="' . esc_url( site_url( 'wp-login.php' )) . '">
+          <div class="loginTile__body grid-x grid-margin-x">
+            <div class="large-4 large-offset-4 cell">
+              <input type="text" placeholder="Username or Email Address" class="loginTile__input" name="user_login" required>
             </div>
-          </div>
-          <div class="loginTile__body cell large-4 large-offset-4 medium-8 medium-offset-2 small-12">
-              <input type="text" placeholder="Email" class="loginTile__input" name="log">
-              <input type="password" placeholder="Password" class="loginTile__input" name="pwd">
+            <div class="large-4 large-offset-4 cell">
+              <input type="password" placeholder="Password" class="loginTile__input" name="user_login" required>
+            </div>
+            <div class="large-2 large-offset-4 cell">
               <button type="reset" class="btn">Clear</button>
+            </div>
+            <div class="large-2 cell">
               <button type="submit" class="btn primaryButton">Login</button>
+            </div>
           </div>
         </form>
       </div>
-      ';
-    }
+    ';
     echo $args['after_widget'];
   }
 }
