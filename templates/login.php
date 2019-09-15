2c944ed26fc2ad2454fdbb7b30d3b891397ffd4f
MelodicNoob <jryan8831@gmail.com>
2019-09-15 12:55:30 +0800

    Added a login and register template file

---
+++ templates/login.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+  Template Name: Alumnus Login
+*/
+
+get_header(); ?>
+
+<?php the_widget('Alumnus_Login_Form'); ?>
+
+<?php get_footer(); ?>
---
+++ templates/register.php
@@ -0,0 +1,10 @@
+<?php
+/*
+  Template Name: Alumnus Registration
+*/
+
+get_header(); ?>
+
+<?php the_widget( 'Alumnus_Register_Form' ); ?>
+
+<?php get_footer(); ?>
