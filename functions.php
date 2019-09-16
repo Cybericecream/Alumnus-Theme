a8f5c60db1a923fc78499cc55076222b0287ed50
MelodicNoob <jryan8831@gmail.com>
2019-09-16 09:09:19 +0800

    Updated functions.php

--- functions.php
+++ functions.php
@@ -59,15 +59,26 @@ add_action( 'widgets_init', 'register_widgets' );
 
 function redirect_login_page() {
     $login_page = home_url('/login');
-    $paged_viewed = basename($_SERVER['REQUEST_URI']);
+    $page_viewed = basename($_SERVER['REQUEST_URI']);
 
-    if ($paged_viewed == 'wp-login.php' && $_SERVER['REQUEST_METHOD'] == 'GET') {
+    if ($page_viewed == 'wp-login.php' && $_SERVER['REQUEST_METHOD'] == 'GET') {
         wp_redirect($login_page);
         exit;
     }
 }
 
+function redirect_register_page() {
+    $register_page = home_url('/register');
+    $page_viewed = basename($_SERVER['REQUEST_URI']);
+
+    if ($page_viewed == 'wp-login.php?action=register' && $_SERVER['REQUEST_METHOD'] == 'GET') {
+        wp_redirect($register_page);
+        exit;
+    }
+}
+
 add_action('init', 'redirect_login_page');
+add_action('init', 'redirect_register_page');
 
 function custom_login_failed() {
     $login_page = home_url('/login');
@@ -85,7 +96,16 @@ function logout_redirect() {
 
 add_action('wp_logout', 'logout_redirect');
 
-function auto_login_registered_user( $user_id ) {
+function registered_user_setup( $user_id ) {
+    if ( isset( $_POST['first_name'] ) ) {
+        update_user_meta( $user_id, 'first_name', sanitize_text_field( $_POST['first_name'] ) );
+    }
+    if ( isset( $_POST['last_name'] ) ) {
+        update_user_meta( $user_id, 'last_name', sanitize_text_field( $_POST['last_name'] ) );
+    }
+    if ( isset( $_POST['yearGraduated'] ) ) {
+        update_user_meta( $user_id, 'yearGraduated', sanitize_text_field( $_POST['yearGraduated'] ) );
+    }
     wp_set_current_user($user_id);
     wp_set_auth_cookie($user_id);
     $user = get_user_by( 'id', $user_id );
@@ -93,4 +113,4 @@ function auto_login_registered_user( $user_id ) {
     wp_redirect( home_url() );
     exit;
 }
-add_action( 'user_register', 'auto_login_registered_user' );
+add_action( 'user_register', 'registered_user_setup' );
