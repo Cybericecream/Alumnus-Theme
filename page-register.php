e3b1d8cb20a458348fe705eed15e3bd69a026725
MelodicNoob <jryan8831@gmail.com>
2019-09-22 19:15:08 +0800

    Removed page-register.php

--- page-register.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-get_header(); ?>
-
-<?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>    
-	<div class="form-background-image" style="background: url('<?php echo $backgroundImg[0]; ?>') no-repeat;">
-		<?php the_widget( 'Alumnus_Register_Form' ); ?>
-	</div> 
-<?php get_footer(); ?>
