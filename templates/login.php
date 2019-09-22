0e2ad9c391f86b70c3e51c41f19794c68a19d64a
MelodicNoob <jryan8831@gmail.com>
2019-09-22 19:17:25 +0800

    Added a login template

---
+++ templates/login.php
@@ -0,0 +1,15 @@
+<?php 
+/** 
+* Template Name: Login
+*/ 
+ob_start();
+get_header();
+?>
+	<!-- Custom Login functionality -->
+	<?php require_once dirname(__FILE__) . '\..\functions\login.php'; ?>
+	<?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); ?>
+	<div class="form-background-image" style="background: url('<?php echo $backgroundImg[0]; ?>') no-repeat;">
+		<?php the_widget( 'Alumnus_Login_Form' ); ?>
+	</div> 
+<?php get_footer(); ?>
+
