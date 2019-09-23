93051a53a23d27b20bbea2d87601497d903e38eb
MelodicNoob <jryan8831@gmail.com>
2019-09-23 12:28:33 +0800

    Updated login template

--- templates/login.php
+++ templates/login.php
@@ -7,7 +7,6 @@ get_header();
 ?>
 	<!-- Custom Login functionality -->
 	<?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); ?>
-	
 	<div class="form-background-image" style="background: url('<?php echo $backgroundImg[0]; ?>') no-repeat;">
 		<?php the_widget( 'Alumnus_Login_Form' ); ?>
 	</div> 
