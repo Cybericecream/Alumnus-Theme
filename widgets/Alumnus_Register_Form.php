7aa96fb8e052e6e825d90e8c0e96a51e82adea70
MelodicNoob <jryan8831@gmail.com>
2019-09-23 14:42:14 +0800

    Updated Alumnus register form widget

--- widgets/Alumnus_Register_Form.php
+++ widgets/Alumnus_Register_Form.php
@@ -49,7 +49,7 @@ if (! class_exists('Alumnus_Register_Form')) {
                     <button type="reset" class="btn">Clear</button>
                 </div>
                 <div class="large-2 cell">
-                    <button type="submit" class="btn primaryButton">Register</button>
+                    <button type="submit" class="btn primaryButton" value="true" name="submit">Register</button>
                 </div>
                 <input type="hidden" name="redirect_to" value="' . esc_url( site_url( '' )) .'">
                 </div>
