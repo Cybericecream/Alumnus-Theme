b6cbc9da62097b755d73d2a3e9f64c04e9dd3b56
MelodicNoob <jryan8831@gmail.com>
2019-09-23 14:41:11 +0800

    Updated Alumnus register

--- functions/alumnus_register.php
+++ functions/alumnus_register.php
@@ -1,4 +1,12 @@
 <?php 
-  if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
+  if ( $_POST['submit'] ) {
     global $wpdb;
+    $username = $wpdb->escape($_POST['user_login']); 
+    $email = $wpdb->escape($_POST['user_email']); 
+    $firstName = $wpdb->escape($_POST['first_name']);
+    $lastName = $wpdb->escape($_POST['last_name']);
+    $yearGraduated = $wpdb->escape($_POST['yearGraduated']);
+    
+		echo $yearGraduated;
+
   }
