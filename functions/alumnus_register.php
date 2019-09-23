6c630e0d7daecdbd446d27335d9b06f0c4cf07f3
MelodicNoob <jryan8831@gmail.com>
2019-09-23 14:53:58 +0800

    Updated Alumnus register validation

--- functions/alumnus_register.php
+++ functions/alumnus_register.php
@@ -6,4 +6,40 @@
     $firstName = $wpdb->escape($_POST['first_name']);
     $lastName = $wpdb->escape($_POST['last_name']);
     $yearGraduated = $wpdb->escape($_POST['yearGraduated']);
+
+    // Validation 
+    $formErrors = array();
+    // Username validation
+    if ( empty( $username ) ) {
+      $errorObject = array(
+				"message" => "Username is empty"
+			);
+			array_push( $formErrors, $errorObject );
+    }
+
+    // Email validation
+    if ( empty( $email ) ) {
+      $errorObject = array(
+        "message" => "Email is empty"
+      );
+      array_push ( $formErrors, $errorObject );
+    }
+
+    if (! filter_var( $email, FILTER_VALIDATE_EMAIL ) ) {
+      $errorObject = array(
+        "message" => "Email is not valid"
+      );
+      array_push ( $formErrors, $errorObject );
+    }
+
+    if ( count( $formErrors ) > 0 ) {
+      // Do something with the errors.
+      foreach ( $formErrors as $key => $errorObject ) {
+				foreach ( $errorObject as $key => $value ) {
+					echo '<div class="error-message">' . $value . '</div>';
+				}
+			}
+    } else {
+      // Register the user
+    }
   }
