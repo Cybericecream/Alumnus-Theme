2325ef5c2041494ee9b025d8daefb825949a7087
MelodicNoob <jryan8831@gmail.com>
2019-10-16 10:51:45 +0800

    Updated Alumnus Login Form

--- widgets/Alumnus_Login_Form.php
+++ widgets/Alumnus_Login_Form.php
@@ -9,20 +9,31 @@ if (! class_exists('Alumnus_Login_Form')) {
             parent::__construct( 'Alumnus_Login_Form', 'Alumnus Login Form', $widget_options);
         }
         public function widget( $args, $instance ) {
+            if (isset($instance['title'])) {
+                $title = apply_filters( 'widget_title', $instance['title'] );
+            }
             echo $args['before_widget'];
         ?>
-            <div class="grid-container">	
+            
+            <div class="grid-container">
+                <?php if (isset($instance['title'])) { ?>
+                    <div class="loginTile__title">
+                        <div class="active">
+                            <h2><?php echo $args['before_title'] . $title . $args['after_title']; ?></h2>
+                        </div>
+                    </div>
+                <?php } ?>
                 <?php require_once get_template_directory() . '/functions/alumnus_login.php' ?>
                 <form name="loginForm" id="loginForm" method="POST" action="<?php echo esc_url( site_url( '/login' )) ?>">
                 <div class="loginTile__body grid-x grid-margin-x">
                     <div class="large-4 large-offset-4 cell">
-                    <input type="text" placeholder="Username or Email Address" class="loginTile__input" name="log">
+                        <input type="text" placeholder="Username or Email Address" class="loginTile__input" name="log">
                     </div>
                     <div class="large-4 large-offset-4 cell">
-                    <input type="password" placeholder="Password" class="loginTile__input" name="pwd">
+                        <input type="password" placeholder="Password" class="loginTile__input" name="pwd">
                     </div>
                     <div class="large-2 large-offset-4 cell">
-                    <button type="reset" class="btn">Clear</button>
+                        <button type="reset" class="btn">Clear</button>
                     </div>
                     <div class="large-2 cell">
                         <button type="submit" class="btn primaryButton">Login</button>
@@ -34,6 +45,27 @@ if (! class_exists('Alumnus_Login_Form')) {
             </div>
         <?php
             echo $args['after_widget'];
+        }
+
+        public function form( $instance ) {
+            if ( isset( $instance[ 'title' ] )) {
+                $title = $instance[ 'title' ];
+            }
+            else {
+                $title = __( 'New title', '' );
+            }
+            ?>
+            <p>
+                <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+                <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+            </p>
+            <?php
+        }
+
+        public function update( $new_instance, $old_instance ) {
+            $instance = array();
+            $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+            return $instance;
         }
     }
 }
