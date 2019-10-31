d04507d48a5806943f5864be999038ad9337de02
Mangusta <Mangusta@Nathans-Air.lan>
2019-10-31 13:49:51 +0800

    Update About - add feature image
    Functionality for featured image

--- templates/about.php
+++ templates/about.php
@@ -4,6 +4,8 @@
 */ 
 ob_start();
 get_header();
+global $post;
+
 ?>
 	<!-- Custom Registration functionality -->
 	<?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>    
@@ -17,11 +19,11 @@ get_header();
 						<div class="cell small-12">
 							<div class="grid-x">
 							<div class="cell medium-5 small-12 ">
-								<img src="images/group.png" />
+								<img src="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' )[0];?>  " />
 							</div>
 							<div class="cell medium-7 small-12 widgetBlock aboutText">
-								<h2><?php single_post_title(); ?></h2>
-								<p><?php get_post()->post_content_filtered; ?></p>
+								<h2><?php echo $post->post_title; ?></h2>
+								<p><?php echo $post->post_content; ?></p>
 							</div>
 						</div>
 					</div>
