3b917afc172f5c1980fa619160c4b8efd94420dd
Cybericecream <cybericecream99@gmail.com>
2019-10-31 13:22:35 +0800

    Fixed all known navigation issues
    :P

--- parts/alumni-custom-post-types.php
+++ parts/alumni-custom-post-types.php
@@ -28,4 +28,32 @@ function carousel_post() {
     );
     register_post_type( 'slider', $args ); 
   }
-  add_action( 'init', 'carousel_post' );
+
+  function event_post() {
+    $labels = array(
+      'name'               => _x( 'Event Listing', 'post type general name' ),
+      'singular_name'      => _x( 'Event List', 'post type singular name' ),
+      'add_new'            => _x( 'Add Event', 'Events' ),
+      'add_new_item'       => __( 'Add New Event' ),
+      'edit_item'          => __( 'Edit Events' ),
+      'new_item'           => __( 'New Events' ),
+      'all_items'          => __( 'All Event Item\'s' ),
+      'view_item'          => __( 'View Event Item\'s' ),
+      'search_items'       => __( 'Search Events Item\'s' ),
+      'not_found'          => __( 'No Envent found' ),
+      'not_found_in_trash' => __( 'No Event found in the Trash' ), 
+      'parent_item_colon'  => '',
+      'menu_name'          => 'Event'
+    );
+    $args = array(
+      'description'   => 'Holds the Items for the Events List',
+      'public'        => true,
+      'menu_position' => 5,
+      'menu_icon' => 'dashicons-calendar-alt',
+      'supports'      => array( 'title', 'thumbnail' ),
+      'has_archive'   => true,
+    );
+    register_post_type( 'events', $args ); 
+  }
+  
+  add_action( 'init', 'carousel_post', 'event_post' );
