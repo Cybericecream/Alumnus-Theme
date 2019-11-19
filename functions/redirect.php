efa48345fdfab91f91f5695294616bcd0f90d6ac
MelodicNoob <jryan8831@gmail.com>
2019-11-19 14:02:28 +0800

    Updated redirect.php

--- functions/redirect.php
+++ functions/redirect.php
@@ -3,9 +3,15 @@
 function redirect_logged_in_users() {
     if (is_user_logged_in()) {
         $home_page = home_url();
-        $page_viewed = basename($_SERVER['REQUEST_URI']);
-        if ($page_viewed == 'register' || $page_viewed == 'login') {
+        $page_viewed = $_SERVER['REQUEST_URI'];
+        if ($page_viewed == '/register' || $page_viewed == '/login') {
             wp_redirect($home_page);
+            exit;
+        }
+
+        if ($page_viewed == '/')
+        {
+            wp_redirect('/dashboard');
             exit;
         }
     }
